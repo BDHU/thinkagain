824893d30a9466696bd28fb8012210515db25db5
BDHU <bodunhu@utexas.edu>
2026-01-13 22:18:12 -0600

    refactor: remove deprecated core and distributed modules (moved to new structure)

--- thinkagain/distributed/grpc/__init__.py
+++
@@ -1,6 +0,0 @@
-"""gRPC backend for distributed execution."""
-
-from .client import GrpcClient
-from .server import ReplicaServicer
-
-__all__ = ["GrpcClient", "ReplicaServicer"]
--- thinkagain/distributed/grpc/__main__.py
+++
@@ -1,6 +0,0 @@
-"""Entry point for python -m thinkagain.distributed.grpc."""
-
-from .serve import main
-
-if __name__ == "__main__":
-    main()
